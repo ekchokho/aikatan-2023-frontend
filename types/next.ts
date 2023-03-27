b93b06f3123d464c4865cf04bb28a5d0519c39d5
bivas-biswas <bivasbiswas1999@gmail.com>
2023-03-28 01:43:32 +0530

    :rotating_light: fixed lint error

--- types/next.ts
+++ types/next.ts
@@ -1,7 +1,8 @@
-import { LayoutProps } from 'components'
 import { NextPage } from 'next'
 import { AppProps as NextAppProps } from 'next/app'
 import { ReactElement } from 'react'
+
+import { LayoutProps } from '@/layout'
 
 export type AppPage = NextPage & {
   (): ReactElement
