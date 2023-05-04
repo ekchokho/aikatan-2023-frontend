9f769fdc4e58986daba7e8983270399e6e25d89c
bivas-biswas <bivasbiswas1999@gmail.com>
2023-05-04 12:02:37 +0530

    :fire: update

--- features/Home/components/ImageCarousel.tsx
+++ features/Home/components/ImageCarousel.tsx
@@ -1,6 +1,5 @@
 import React, { Suspense } from 'react'
 
-import { Preload } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 
 const ImageCarousel = ({ children }: { children: React.ReactNode }) => {
@@ -8,8 +7,6 @@ const ImageCarousel = ({ children }: { children: React.ReactNode }) => {
     <div className="top-0 left-0 absolute w-full h-screen">
       <Canvas>
         <Suspense fallback={null}>{children}</Suspense>
-
-        <Preload all />
       </Canvas>
     </div>
   )
